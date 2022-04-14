9d6b32619b20da3598227225abeb7e076b0a4835
Nancyzxy <11912039@mail.sustech.edu.cn>
2022-04-14 20:21:07 +0800

    Regular matching.

---
+++ RegularMatching/clean.py
@@ -0,0 +1,296 @@
+import pandas as pd
+import re
+
+brands = ['dell', 'lenovo', 'acer', 'asus', 'hp']
+
+cpu_brands = ['intel', 'amd']
+
+intel_cores = [' i3', ' i5', ' i7', '2 duo', 'celeron', 'pentium', 'centrino']
+amd_cores = ['e-series', 'a8', 'radeon', 'athlon', 'turion', 'phenom']
+
+families = {
+    'hp': [r'elitebook', r'compaq', r'folio', r'pavilion'],
+    'lenovo': [r' x[0-9]{3}[t]?', r'x1 carbon'],
+    'dell': [r'inspiron'],
+    'asus': [r'zenbook', ],
+    'acer': [r'aspire', r'extensa', ],
+    '0': []
+}
+
+
+def clean_x2(data):
+    """Clean X2.csv data to a readable format.
+
+    :param data: X2.csv
+
+    :return:
+        A DataFrame which contains following columns:
+        {instance_id: instance_id of items;
+         brand: computer's brand, range in: {'dell', 'lenovo', 'acer', 'asus', 'hp'};
+         cpu_brand: cpu's brand, range in: {'intel', 'amd'};
+         cpu_core: cpu extra information, relative to cpu_brand;
+         cpu_model: cpu model, relative to cpu_brand;
+         cpu_frequency: cpu's frequency, unit in Hz;
+         ram_capacity: capacity of RAM, unit in GB;
+         display_size: size of computer;
+         pc_name: name information extract from title;
+         name_family: family name of computer;
+         title: title information of instance}
+
+         it the value can't extract from the information given, '0' will be filled.
+    """
+
+    instance_ids = data.filter(items=['id'], axis=1)
+    titles = data.filter(items=['title'], axis=1)
+    information = data.drop(["id"], axis=1)
+    information = information.fillna('')
+    instance_ids = instance_ids.values.tolist()
+    information = information.values.tolist()
+    titles = titles.values.tolist()
+
+    result = []
+    for row in range(len(instance_ids)):
+        # information按长度排序
+        #information[row] =list(map(lambda x:str(x),information[row]))
+        information[row].sort(key=lambda i: len(i), reverse=True)
+        # title of each row
+        if len(titles[row]) == 0:
+            titles[row] = ['']
+        rowinfo = titles[row][0]
+
+        #对丢失的信息进行填充
+        for mess in information[row]:
+            if mess not in rowinfo:
+                rowinfo = rowinfo + ' - ' + mess
+        brand = '0'
+        cpu_brand = '0'
+        cpu_core = '0'
+        cpu_model = '0'
+        cpu_frequency = '0'
+        ram_capacity = '0'
+        display_size = '0'
+        name_number = '0'
+        name_family = '0'
+
+        item = rowinfo
+        lower_item = item.lower()
+
+        name_info = item
+
+        for b in brands:
+            if b in lower_item:
+                brand = b
+                break
+
+        for b in cpu_brands:
+            if b in lower_item:
+                cpu_brand = b
+                break
+        if cpu_brand != 'intel':
+            for b in amd_cores:
+                if b in lower_item:
+                    cpu_core = b.strip()
+                    #补充信息
+                    cpu_brand = 'amd'
+                    break
+        if cpu_brand != 'amd':
+            for b in intel_cores:
+                if b in lower_item:
+                    cpu_core = b.strip()
+                    cpu_brand = 'intel'
+                    break
+
+        if brand == 'lenovo':
+            #print(name_info)
+            result_name_number = re.search(
+                r'[\- ][0-9]{4}[0-9a-zA-Z]{2}[0-9a-yA-Y](?![0-9a-zA-Z])', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(
+                    r'[\- ][0-9]{4}(?![0-9a-zA-Z])', name_info)
+                #print(result_name_number.group())
+            if result_name_number is not None:
+                #print(name_info)
+                #print(result_name_number.group())
+                name_number = result_name_number.group().replace(
+                    '-', '').strip().lower()[:4]
+        elif brand == 'hp':
+            #print(name_info)
+            result_name_number = re.search(r'[0-9]{4}[pPwW]', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(
+                    r'15[\- ][a-zA-Z][0-9]{3}[a-zA-Z]{2}', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(r'[\s]810[\s](G2)?', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(r'[0-9]{4}[mM]', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(
+                    r'((DV)|(NC))[0-9]{4}', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(r'[0-9]{4}DX', name_info)
+            if result_name_number is not None:
+                #print(result_name_number.group())
+                name_number = result_name_number.group().lower().replace('-', '').replace(' ', '')
+        elif brand == 'dell':
+            #print(name_info)
+            result_name_number = re.search(
+                r'[a-zA-Z][0-9]{3}[a-zA-Z]', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(r'[0-9]{3}-[0-9]{3}', name_info)
+            if result_name_number is not None:
+                #print(result_name_number.group())
+                name_number = result_name_number.group().lower().replace('-', '')
+        elif brand == 'acer':
+            #print(name_info)
+            result_name_number = re.search(
+                r'[A-Za-z][0-9][\- ][0-9]{3}', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(r'AS[0-9]{4}', name_info)
+            if result_name_number is None:
+                result_name_number = re.search(
+                    r'[0-9]{4}[- ][0-9]{4}', name_info)
+            if result_name_number is not None:
+                #print(result_name_number.group())
+                name_number = result_name_number.group().lower().replace(' ', '-').replace('-', '')
+                if len(name_number) == 8:
+                    #print(name_number)
+                    name_number = name_number[:4]
+        elif brand == 'asus':
+            #print(name_info)
+            result_name_number = re.search(
+                r'[A-Za-z]{2}[0-9]?[0-9]{2}[A-Za-z]?[A-Za-z]', name_info)
+            if result_name_number is not None:
+                #print(result_name_number.group())
+                name_number = result_name_number.group().lower().replace(' ', '-').replace('-', '')
+
+        if cpu_brand == 'intel':
+            item_curr = item.replace(
+                name_number, '').replace(
+                name_number.upper(), '')
+            #print(item_curr)
+            result_model = re.search(
+                r'[\- ][0-9]{4}[Qq]?[MmUu](?![Hh][Zz])', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ][0-9]{3}[Qq]?[Mm]', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ][MmQq][0-9]{3}', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ][PpNnTt][0-9]{4}', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ][0-9]{4}[Yy]', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ][Ss]?[Ll][0-9]{4}', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ]867', item_curr)
+            if result_model is None:
+                result_model = re.search(
+                    '[\\- ]((1st)|(2nd)|(3rd)|([4-9]st))[ ][Gg]en', item_curr)
+            if result_model is not None:
+                cpu_model = result_model.group()[1:].lower()
+                #print(cpu_model)
+        elif cpu_brand == 'amd':
+            item_curr = item.replace(
+                name_number, '').replace(
+                name_number.upper(), '')
+            if cpu_core == 'a8':
+                cpu_core = 'a-series'
+            result_model = re.search(r'([AaEe][0-9][\- ][0-9]{4})', item_curr)
+            if result_model is None:
+                result_model = re.search('[\\- ]HD[\\- ][0-9]{4}', item_curr)
+            if result_model is None:
+                result_model = re.search(
+                    '[\\- ][AaEe][\\- ][0-9]{3}', item_curr)
+            if result_model is not None:
+                cpu_core = result_model.group().replace(
+                    '-', '').replace(' ', '')[:1].lower() + '-series'
+                cpu_model = result_model.group()[1:].lower().replace(' ', '-')
+            if cpu_core in ('radeon', 'athlon', 'turion', 'phenom'):
+                if result_model is None:
+                    result_model = re.search('[\\- ][NnPp][0-9]{3}', item_curr)
+                if result_model is None:
+                    result_model = re.search(
+                        '[\\- ](64[ ]?[Xx]2)|([Nn][Ee][Oo])', item_curr)
+                if result_model is not None:
+                    cpu_model = result_model.group().lower().replace('-', '').replace(' ', '')
+
+        result_frequency = re.search(
+            r'[123][ .][0-9]?[0-9]?[ ]?[Gg][Hh][Zz]', item)
+        if result_frequency is not None:
+            #print(result_frequency.group())
+            result_frequency = re.split(r'[GgHhZz]', result_frequency.group())[
+                0].strip().replace(' ', '.')
+            if len(result_frequency) == 3:
+                result_frequency = result_frequency + '0'
+            if len(result_frequency) == 1:
+                result_frequency = result_frequency + '.00'
+            result_frequency = result_frequency
+            cpu_frequency = result_frequency
+
+        result_ram_capacity = re.search(
+            r'[1-9][\s]?[Gg][Bb][\s]?((S[Dd][Rr][Aa][Mm])|(D[Dd][Rr]3)|([Rr][Aa][Mm])|(Memory))', item)
+        if result_ram_capacity is not None:
+            #print(result_ram_capacity.group())
+            ram_capacity = result_ram_capacity.group()[:1]
+
+        result_display_size = re.search(r'1[0-9]([. ][0-9])?\"', item)
+        if result_display_size is not None:
+            display_size = result_display_size.group().replace(" ", ".")[:-1]
+        else:
+            result_display_size = re.search(
+                r'1[0-9]([. ][0-9])?[- ][Ii]nch(?!es)', item)
+        if result_display_size is not None and display_size == '0':
+            display_size = result_display_size.group().replace(" ", ".")[:-5]
+        elif result_display_size is None:
+            result_display_size = re.search(
+                r'(?<!x)[ ]1[0-9][. ][0-9]([ ]|(\'\'))(?!x)', item)
+        if result_display_size is not None and display_size == '0':
+            display_size = result_display_size.group().replace(
+                "\'", " ").strip().replace(' ', '.')
+            #print(item)
+            #print(display_size)
+
+        for pattern in families[brand]:
+            result_name_family = re.search(pattern, lower_item)
+            if result_name_family is not None:
+                name_family = result_name_family.group().strip()
+                break
+
+        result.append([
+            instance_ids[row][0],
+            brand,
+            cpu_brand,
+            cpu_core,
+            cpu_model,
+            cpu_frequency,
+            ram_capacity,
+            display_size,
+            name_number,
+            name_family,
+            titles[row][0].lower()
+        ])
+        #print(result[len(result)-1])
+        #result[0].sort(key=lambda i: len(i), reverse=True)
+        # for x in result[0]:
+        #     r = re.search(x,item.lower())
+        #     if r is not None:
+        #         item = item.lower().replace(x,"")
+
+
+    result = pd.DataFrame(result)
+    name = [
+        'instance_id',
+        'brand',
+        'cpu_brand',
+        'cpu_core',
+        'cpu_model',
+        'cpu_frequency',
+        'ram_capacity',
+        'display_size',
+        'pc_name',
+        'family',
+        'title'
+    ]
+    for i in range(len(name)):
+        result.rename({i: name[i]}, inplace=True, axis=1)
+
+    return result
---
+++ RegularMatching/run.py
@@ -0,0 +1,178 @@
+import csv
+
+from handler import handle
+from collections import defaultdict
+from tqdm import tqdm
+import pandas as pd
+import re
+
+Flag = True
+
+#
+#
+def block_with_attr(X, attr):  # replace with your logic.
+    '''
+    This function performs blocking using attr
+    :param X: dataframe
+    :param attr: attribute used for blocking
+    :return: candidate set of tuple pairs
+    '''
+
+    # build index from patterns to tuples
+    pattern2id_1 = defaultdict(list)
+    pattern2id_2 = defaultdict(list)
+    pattern2id_3 = defaultdict(list)
+    pattern2id_4 = defaultdict(list)
+    for i in tqdm(range(X.shape[0])):
+        attr_i = str(X[attr][i])
+        pattern_1 = " ".join(sorted(attr_i.lower().split()))  # use the whole attribute as the pattern
+        pattern2id_1[pattern_1].append(i)
+
+        pattern_2 = re.findall("\w+\s\w+\d+", attr_i)  # look for patterns like "thinkpad x1"
+        if len(pattern_2) != 0:
+            pattern_2 = list(sorted(pattern_2))
+            pattern_2 = [str(it).lower() for it in pattern_2]
+            pattern2id_2[" ".join(pattern_2)].append(i)
+
+        pattern_3=[]
+        pattern_3_tmp = re.findall("\w+-\w+",attr_i)
+        for x in pattern_3_tmp:
+            if "bit" in x.lower():
+                continue
+            pattern_3.append(x)
+        if len(pattern_3) != 0:
+            pattern_3 = list(sorted(pattern_3))
+            pattern_3 = [str(it).lower() for it in pattern_3]
+            pattern2id_3[" ".join(pattern_3)].append(i)
+
+        pattern_4_tmp = re.findall("\d+\w+", attr_i)
+        pattern_4 = []
+        for x in pattern_4_tmp:
+            if len(x) >= 4:
+                pattern_4.append(x)
+        if len(pattern_4) != 0:
+            pattern_4= list(sorted(pattern_4))
+            pattern_4 = [str(it).lower() for it in pattern_4]
+            pattern2id_4[" ".join(pattern_4)].append(i)
+    # add id pairs that share the same pattern to candidate set
+    candidate_pairs_1 = []
+    for pattern in tqdm(pattern2id_1):
+        ids = list(sorted(pattern2id_1[pattern]))
+        for i in range(len(ids)):
+            for j in range(i + 1, len(ids)):
+                candidate_pairs_1.append((ids[i], ids[j]))  #
+    # add id pairs that share the same pattern to candidate set
+    candidate_pairs_2 = []
+    for pattern in tqdm(pattern2id_2):
+        ids = list(sorted(pattern2id_2[pattern]))
+        if len(ids) < 100:  # skip patterns that are too common
+            for i in range(len(ids)):
+                for j in range(i + 1, len(ids)):
+                    candidate_pairs_2.append((ids[i], ids[j]))
+
+    candidate_pairs_3 = []
+    for pattern in tqdm(pattern2id_3):
+        ids = list(sorted(pattern2id_3[pattern]))
+        if len(ids) < 100:  # skip patterns that are too common
+            for i in range(len(ids)):
+                for j in range(i + 1, len(ids)):
+                    candidate_pairs_3.append((ids[i], ids[j]))
+
+    candidate_pairs_4 = []
+    for pattern in tqdm(pattern2id_4):
+        ids = list(sorted(pattern2id_4[pattern]))
+        if len(ids) < 100:  # skip patterns that are too common
+            for i in range(len(ids)):
+                for j in range(i + 1, len(ids)):
+                    candidate_pairs_4.append((ids[i], ids[j]))
+
+
+    # remove duplicate pairs and take union
+    candidate_pairs = set(candidate_pairs_2)
+    candidate_pairs = candidate_pairs.union(set(candidate_pairs_1)).union(set(candidate_pairs_3)).union(set(candidate_pairs_4))
+    candidate_pairs = list(candidate_pairs)
+
+    # sort candidate pairs by jaccard similarity.
+    # In case we have more than 1000000 pairs (or 2000000 pairs for the second dataset),
+    # sort the candidate pairs to put more similar pairs first,
+    # so that when we keep only the first 1000000 pairs we are keeping the most likely pairs
+    jaccard_similarities = []
+    candidate_pairs_real_ids = []
+    for it in tqdm(candidate_pairs):
+        id1, id2 = it
+
+        # get real ids
+        real_id1 = X['id'][id1]
+        real_id2 = X['id'][id2]
+        if real_id1 < real_id2:  # NOTE: This is to make sure in the final output.csv, for a pair id1 and id2 (assume id1<id2), we only include (id1,id2) but not (id2, id1)
+            candidate_pairs_real_ids.append((real_id1, real_id2))
+        else:
+            candidate_pairs_real_ids.append((real_id2, real_id1))
+
+        # compute jaccard similarity
+        name1 = str(X[attr][id1])
+        name2 = str(X[attr][id2])
+        s1 = set(name1.lower().split())
+        s2 = set(name2.lower().split())
+        jaccard_similarities.append(len(s1.intersection(s2)) / max(len(s1), len(s2)))
+    candidate_pairs_real_ids = [x for _, x in sorted(zip(jaccard_similarities, candidate_pairs_real_ids), reverse=True)]
+    return candidate_pairs_real_ids
+
+
+def save_output(X1_candidate_pairs,
+                X2_candidate_pairs):  # save the candset for both datasets to a SINGLE file output.csv
+    expected_cand_size_X1 = 1000000
+    expected_cand_size_X2 = 2000000
+
+    # make sure to include exactly 1000000 pairs for dataset X1 and 2000000 pairs for dataset X2
+    if len(X1_candidate_pairs) > expected_cand_size_X1:
+        X1_candidate_pairs = X1_candidate_pairs[:expected_cand_size_X1]
+    if len(X2_candidate_pairs) > expected_cand_size_X2:
+        X2_candidate_pairs = X2_candidate_pairs[:expected_cand_size_X2]
+
+    # make sure to include exactly 1000000 pairs for dataset X1 and 2000000 pairs for dataset X2
+    if len(X1_candidate_pairs) < expected_cand_size_X1:
+        X1_candidate_pairs.extend([(0, 0)] * (expected_cand_size_X1 - len(X1_candidate_pairs)))
+    if len(X2_candidate_pairs) < expected_cand_size_X2:
+        X2_candidate_pairs.extend([(0, 0)] * (expected_cand_size_X2 - len(X2_candidate_pairs)))
+
+    all_cand_pairs = X1_candidate_pairs + X2_candidate_pairs  # make sure to have the pairs in the first dataset first
+    output_df = pd.DataFrame(all_cand_pairs, columns=["left_instance_id", "right_instance_id"])
+    # In evaluation, we expect output.csv to include exactly 3000000 tuple pairs.
+    # we expect the first 1000000 pairs are for dataset X1, and the remaining pairs are for dataset X2
+    output_df.to_csv("output.csv", index=False)
+
+if __name__ == '__main__':
+    X1_candidate_pairs = []
+    X2_candidate_pairs = []
+    for file_name in ['X1.csv','X2.csv']:
+        if file_name == "X1.csv":
+            X1 = pd.read_csv("X1.csv")
+            X1_candidate_pairs = block_with_attr(X1, attr="title")
+            data = pd.read_csv(file_name)
+            X1_set = set(X1_candidate_pairs)
+            X11_candidate_pairs = handle(data).values.tolist()
+            X11_candidate_pairs = map(lambda x:tuple(x),X11_candidate_pairs)
+            X11_set = set(X11_candidate_pairs)
+            X1_candidate_pairs = list(X1_set.union(X11_set))
+        else:
+            # read the datasets
+            X2 = pd.read_csv("X2.csv",dtype=object)
+            # perform blocking
+            X2_candidate_pairs = block_with_attr(X2, attr="name")
+
+            # save results
+        save_output(X1_candidate_pairs, X2_candidate_pairs)
+        # if Flag:
+        #         output.to_csv("Y1.csv", sep=',', encoding='utf-8', index=False)
+        #         Flag = False
+
+    #calculate
+    # with open('../Y1.csv', 'r') as csv1, open('output.csv', 'r') as csv2:
+    #     import1 = csv1.readlines()
+    #     import2 = csv2.readlines()
+    #     same = 0
+    #     for row in import2:
+    #         if row in import1:
+    #             same = same + 1
+    #     print(same / len(import1))
